f1c6ca15fe9895852f4ae31b817f3c17315e588d
Johnatan <Johnatan@PikachuDoMal>
2021-04-04 22:56:32 -0300

    Alterado

--- scripts/script_vacinas.sql
+++ scripts/script_vacinas.sql
@@ -4,11 +4,11 @@ use atividadeVacinas;
 
 create table vacina(
 	idVacina int not null auto_increment primary key,
-    nomeVacina varchar(50) not null,
+    nome varchar(50) not null,
 	paisOrigem varchar(50) not null,
-	estagioPesquisa varchar(30) not null,
+	estagioPesquisa varchar(30),
 	dtInicioPesquisa date not null,
-	dtTerminoPesquisa date not null,
+	dtTerminoPesquisa date,
 	quantidadeDoses int not null
 );
 create table aplicacao(
@@ -46,12 +46,18 @@ create table publicoGeral(
     fkIdPessoa int not null,
     foreign key(fkIdPessoa) references pessoa(idPessoa)
 );
-insert into vacina(nomeVacina,paisOrigem,estagioPesquisa,dtInicioPesquisa,dtTerminoPesquisa,quantidadeDoses) values("Sputnic","Russia","Final", '2020-05-05', '2021-01-05', 200);
+insert into vacina(nome,paisOrigem,estagioPesquisa,dtInicioPesquisa,dtTerminoPesquisa,quantidadeDoses) values("Sputnic","Russia","Final", '2020-05-05', '2021-01-05', 200);
 insert into aplicacao(fkIdVacina, dtAplicacao, nota) values(1, '2021-02-10', "Bom");
-insert into pessoa(nome, sobrenome, dtNascimento, sexo, cpf, fkIdAplicacao) values("Zé", "da coca", '2024-08-09', 'M', "123.456.789-12", 1);
+insert into pessoa(nome, sobrenome, dtNascimento, sexo, cpf, fkIdAplicacao) values("Zé", "1", '2000-09-09', 'M', "111.111.111-11", 1);
+insert into pessoa(nome, sobrenome, dtNascimento, sexo, cpf, fkIdAplicacao) values("Zé", "2", '1998-10-10', 'F', "222.222.222-22", 1);
+insert into pessoa(nome, sobrenome, dtNascimento, sexo, cpf, fkIdAplicacao) values("Zé", "3", '1991-11-11', 'F', "333.333.333-33", 1);
 insert into pesquisador(instituicao, fkIdVacinaCriada, fkIdPessoa) values("Apocalipse", 1, 1);
+insert into publicoGeral(fkIdPessoa) values(2);
+insert into voluntario(fkIdPessoa) values(3);
 
 select * from vacina;
-select * from pessoa;
 select * from aplicacao;
+select * from pessoa;
 select * from pesquisador;
+select * from publicoGeral;
+select * from voluntario;
